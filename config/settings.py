eebfa8cf27b3ee15743500faf798088f8ec0dfd6
abdulwahed-mansour <73190425+abdulwahed-mansour@users.noreply.github.com>
2021-08-25 22:32:20 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,15 @@ TEMPLATES = [
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
